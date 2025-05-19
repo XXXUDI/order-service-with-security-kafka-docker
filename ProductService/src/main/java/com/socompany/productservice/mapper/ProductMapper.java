e6da54ea9d5a95d4ce5712a1aaff7e83530e20bd
XXXUDI <sashamlgg@gmail.com>
2025-05-19 20:15:53 +0200

    BugFixed

--- ProductService/src/main/java/com/socompany/productservice/mapper/ProductMapper.java
+++ ProductService/src/main/java/com/socompany/productservice/mapper/ProductMapper.java
@@ -8,18 +8,23 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring", imports = {java.util.UUID.class, java.time.Instant.class})
 public interface ProductMapper {
 
-    @Mapping(target = "id", expression = "java(java.util.UUID.randomUUID())")
-    @Mapping(target = "createdDate", expression = "java(java.time.Instant.now())")
-    @Mapping(target = "lastModifiedDate", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "id", expression = "java(UUID.randomUUID())")
+    @Mapping(target = "createdDate", expression = "java(Instant.now())")
+    @Mapping(target = "lastModifiedDate", expression = "java(Instant.now())")
     @Mapping(source = "name", target = "name")
     @Mapping(source = "price", target = "price")
     @Mapping(source = "description", target = "description")
-    @Mapping(target = "version", constant = "0")
+    @Mapping(target = "version", constant = "0L")
     @Mapping(source = "ownerId", target = "ownerId")
+    @Mapping(source = "quantity", target = "quantity")
     Product toEntity(ProductDto productDto);
 
-    @Mapping(source = "id", target = "uuid")
+    @Mapping(source = "id", target = "id")
     @Mapping(source = "ownerId", target = "ownerId")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "price", target = "price")
+    @Mapping(source = "description", target = "description")
+    @Mapping(source = "quantity", target = "quantity")
     ProductDto toDto(Product product);
-
 }
+
